4c3642e55fcd2701b1d2ed498970e207203442dd
William Martinez Bas <metfar@gmail.com>
2022-05-09 09:30:16 -0300

    Update shellcon.py
    Just for Linux right now

---
+++ shellcon.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  shellcon.py
+#  
+#  Copyright 2019 William Martinez Bas <metfar@gmail.com>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+#  
+
+import sys;
+import subprocess;
+import os;
+import tempfile;
+
+def exists(name):
+	"""
+	File Functions
+	--------------
+	
+	exists('/tmp')
+		returns true if the argument exists
+	"""
+	return(os.path.exists(name));
+	
+def dirExists(name):
+	"""
+	File Functions
+	--------------
+
+	dirExists('/tmp')
+			returns true if it exists and it is a directory
+	"""
+	if(os.path.exists(name)):
+		return(os.path.isdir(name));
+	else:
+		return(False);
+
+def mkdires(arr_dirs=["Temp"]):
+	"""
+	File Functions
+	--------------
+	
+	mkdires()
+		create default Temp directory
+	
+	mkdires(["This","Another"])
+		create desired directories
+	
+	"""
+	global DIRES;
+	for f in ["Temp"]:
+		if not(dirExists(f)): 
+			os.makedirs(f,exist_ok=True);
+		if(not f in DIRES):
+			DIRES.append(f);
+			
+def chmod(name,octal):
+	os.chmod(name,int(str(octal),base=8));
+	
+def execute(cmd):
+	"""
+	OS Function
+	-----------
+	
+	execute("argument")
+	
+		This creates a temporal file to execute with the argument inside.
+		Then, it executes the file and destroys it.
+	"""
+	HEADER="#!/bin/bash\n";
+	FOOT="\n";
+	fname=tempfile.mktemp("tmp");
+	f=open(fname,"w");
+	f.write(HEADER+cmd+FOOT);
+	f.close();
+	chmod(fname,777);
+	subprocess.call(fname);
+	os.unlink(fname);
+	
+def scp(source,target):
+	"""
+		scp(From,To)
+	"""
+	execute("scp "+str(source)+" "+str(target)+" 1>/dev/null 2>/dev/null");
+	#pass;
+
+def file_get_content(name):
+	tmp=open(name,"r");
+	content=tmp.read();
+	return(content);
+
+def main(args):
+	global WIDTH,HEIGHT;
+	vargs=list(args);
+	APP=vargs.pop(0);
+	WIDTH,HEIGHT=os.get_terminal_size();
+	print("*"*WIDTH);
+	form="{:^"+str(WIDTH-4)+"}";
+	for f in range(3,HEIGHT//2-1):
+		print("*"," "*(WIDTH-4),"*");
+	print("*",form.format(APP),"*");
+	for f in range(3,HEIGHT//2-1):
+		print("*"," "*(WIDTH-4),"*");
+	print("*"*WIDTH);
+	print();
+	execute("ls");
+	print();
+	return (0);
+
+if __name__ == '__main__':
+	sys.exit(main(sys.argv));
